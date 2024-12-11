b2ac6f59336cb748649cee21fae6a7428d91debc
vikashbond <vikash.mishra22110@gmail.com>
2024-12-11 15:59:42 +0530

    Update AddressbookUI.java
    commit by vikash

--- src/main/java/com/devopsdemo/tutorial/addressbook/AddressbookUI.java
+++ src/main/java/com/devopsdemo/tutorial/addressbook/AddressbookUI.java
@@ -17,7 +17,7 @@ import com.vaadin.ui.VerticalLayout;
 import com.vaadin.v7.data.util.BeanItemContainer;
 import com.vaadin.v7.ui.Grid;
 import com.vaadin.v7.ui.TextField;
-
+// Hi there comment by Vikash
 /****** User Interface written in Java.
  *
  * Define the user interface shown on the Vaadin generated web page by extending the UI class.
